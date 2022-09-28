edd327fec904ebb02498e2b73b72967f1d54660f
hn-kitty <97036737+hn-kitty@users.noreply.github.com>
2022-09-29 01:20:08 +0800

    Update 137-tk.sh

--- 137-tk.sh
+++ 137-tk.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 wget https://raw.githubusercontent.com/hn-kitty/TAG/main/warp.sh -O warp.sh
-bash warp.sh
+bash warp.sh 4
 
 wget https://raw.githubusercontent.com/hn-kitty/TAG/main/x-ui.sh -O x-ui.sh
 bash x-ui.sh
