eb0df6fc61d4e39d50f94e25a7d5578a5710d6a4
hn-kitty <97036737+hn-kitty@users.noreply.github.com>
2022-09-29 00:42:26 +0800

    Create 181-ml.sh

---
+++ 181-ml.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+wget https://raw.githubusercontent.com/hn-kitty/TAG/main/warp.sh -O warp.sh
+bash warp.sh
+
+wget https://raw.githubusercontent.com/hn-kitty/TAG/main/x-ui.sh -O x-ui.sh
+bash x-ui.sh
+
+x-ui stop
+wget https://github.com/hn-kitty/TAG/blob/main/181-ml-x-ui.db?raw=true -O /etc/x-ui/x-ui.db
+x-ui restart
